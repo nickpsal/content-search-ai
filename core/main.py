e51c032edd0a0207448ced6714481d60e6994de0
NIKOLAOS PSALTAKIS <nickpsal@gmail.com>
2025-08-16 16:23:30 +0300

    remove core main.py because we dont sue it

--- core/main.py
+++
@@ -1,8 +0,0 @@
-from image_search import ImageSearcher
-
-search_query = "Ενα άλογο στην παραλία"
-searcher = ImageSearcher()
-searcher.download_coco_data()
-searcher.extract_image_embeddings()
-searcher.extract_text_embeddings()
-searcher.search(search_query)
