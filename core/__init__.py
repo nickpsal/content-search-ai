f4646432bd0faa49a333fb876e242344e70168df
NIKOLAOS PSALTAKIS <nickpsal@gmail.com>
2025-10-19 15:05:54 +0300

    restore old version

--- core/__init__.py
+++ core/__init__.py
@@ -1,1 +1,2 @@
 from .image_search import ImageSearcher
+#from .model import Model
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from core import ImageSearcher
+#from core import Model
 
-search_query = "Γυναίκα στην παραλία"
+search_query = "Ενα άλογο στην παραλία"
 searcher = ImageSearcher()
 searcher.download_coco_data()
 searcher.extract_image_embeddings()
 searcher.extract_text_embeddings()
 searcher.search(search_query)
+
+
